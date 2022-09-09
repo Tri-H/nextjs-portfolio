d5b5864a94002f5d8cc1608c9272d47b8bed041c
Tri-H <tri.hartanto@outlook.co.id>
2022-09-09 14:52:50 +0700

    refactor: change icon tsx

--- src/components/atoms/icons/Icon.tsx
+++ src/components/atoms/icons/Icon.tsx
@@ -1,46 +1,48 @@
 import {
-	IconLogo,
-	IconSun,
-	IconMoon,
-	IconInstagram,
-	IconTwitter,
-	IconLinkedin,
-	IconGithub,
-	IconVuejs,
-	IconReact,
-	IconNextjs,
-	IconTailwind,
-} from '@/components/atoms/icons'
+  IconLogo,
+  IconSun,
+  IconMoon,
+  IconInstagram,
+  IconTwitter,
+  IconLinkedin,
+  IconGithub,
+  IconVuejs,
+  IconReact,
+  IconNextjs,
+  IconTailwind,
+} from "@/components/atoms/icons";
 
 type IconProps = {
-	name: string
-}
+  name: string;
+};
 
-export default function Icon({ name }: IconProps) {
-	switch (name) {
-		case 'Logo':
-			return <IconLogo />
-		case 'Sun':
-			return <IconSun />
-		case 'Moon':
-			return <IconMoon />
-		case 'Instagram':
-			return <IconInstagram />
-		case 'Twitter':
-			return <IconTwitter />
-		case 'Linkedin':
-			return <IconLinkedin />
-		case 'Github':
-			return <IconGithub />
-		case 'Vue.js':
-			return <IconVuejs />
-		case 'React':
-			return <IconReact />
-		case 'Next.js':
-			return <IconNextjs />
-		case 'Tailwind CSS':
-			return <IconTailwind />
-		default:
-			return <IconLogo />
-	}
-}
+const Icon = ({ name }: IconProps) => {
+  switch (name) {
+    case "Logo":
+      return <IconLogo />;
+    case "Sun":
+      return <IconSun />;
+    case "Moon":
+      return <IconMoon />;
+    case "Instagram":
+      return <IconInstagram />;
+    case "Twitter":
+      return <IconTwitter />;
+    case "Linkedin":
+      return <IconLinkedin />;
+    case "Github":
+      return <IconGithub />;
+    case "Vue.js":
+      return <IconVuejs />;
+    case "React":
+      return <IconReact />;
+    case "Next.js":
+      return <IconNextjs />;
+    case "Tailwind CSS":
+      return <IconTailwind />;
+    default:
+      return <IconLogo />;
+  }
+};
+
+export default Icon;
